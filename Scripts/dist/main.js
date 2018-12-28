04c875a6efbc2ea9d2662b68a3691d7a75d54476
d <d@ms.com>
2018-12-29 00:16:58 +0800

    搭建蜂巢的基本结构 以及 两个基本AI
    Signed-off-by: d <d@ms.com>

--- Scripts/dist/main.js
+++ Scripts/dist/main.js
@@ -1,80 +1,32 @@
-var Minner =  require('role.minner');
-var Builder =  require('role.builder');
-var Harvester =  require('role.harvester');
-var Upgrader =  require('role.upgrader');
-var Fixer = require('role.fixer');
-var Soldier = require('role.soldier');
-var Transfer = require('role.transfer');
+require('config');
+const Queen = require('class.queen');
 
-var Defender = require('sys.defend');
+const queen = new Queen(_PlayerName_, _HomeShard_, _HomeRoom_);
 
-const CR = require('const.creepRole');
-const GV = require('const.gloable');
+(function(){
+    // 初始化整个系统
+    // 为避免污染变量环境，包裹整个函数体
+    queen.junction();
+}());
 
-const Schedule = require('sys.schedule');
 
-const Command = require('function.command');
-
-//Command.eventLog(EVENT_HARVEST);
-//Command.buildRemoteHarvester('Home','E39N47');
 module.exports.loop = function () {
-    
-    Schedule.run();
-    
-    for(var name in Game.creeps) {
-        let creep = Game.creeps[name];
-        // say raomin!
-        if(creep.memory.role == CR.ROLE.Soldier);
-        else if(creep.hits < creep.hitsMax){
-            //creep.moveTo(26, 19, {visualizePathStyle: {stroke: '#FF0000'}});
-            creep.say("😱 好汉饶命!", true);
-            //continue;
-        } else if(creep.ticksToLive < GV.minTickToLive){
-            Command.sendLegancy(creep);
-            continue;
-        }
-        
-        switch(creep.memory.role){
-            case CR.ROLE.Harvester:
-                Harvester.HarvesterAccepter(creep);
-                break;
-            case CR.ROLE.Minner:
-                Minner.run(creep);
-                break;
-            case CR.ROLE.Builder:
-                Builder.run(creep);
-                break;
-            case CR.ROLE.Upgrader:
-                Upgrader.run(creep);
-                break;
-            case CR.ROLE.Fixer:
-                Fixer.FixerAccepter(creep);
-                break;
-            case CR.ROLE.Soldier:
-                Soldier.run(creep);
-                break;
-            case CR.ROLE.Transfer:
-                Transfer.run(creep);
-                break;
-        }
-    }
-    
-    let tower = Game.getObjectById('5c20ab6ef1f0250746ff487c');
-    Defender.run(tower);
-    
-    // Do Command Manualy
-    doCommand(false, false);
-}
 
-function doCommand(outputCpuUsed, excute){
-    let startCpu;
-    if(outputCpuUsed) startCpu = Game.cpu.getUsed();
-    
-    if(excute){
-        // DO Commands here!
-        Command.signAController('E38N46', 'Im thinking about to control this room in a few days.');
-        
+
+    timeSchedule();
+
+    queen.doCommand();
+};
+
+
+function timeSchedule() {
+    if(Game.time % 2 === 0){
+        console.log('checkAllBees');
+        queen.checkAllBees();
     }
-    
-    if(outputCpuUsed)console.log('Command Cpu used:',Game.cpu.getUsed() - startCpu);
+
+    if(Game.time % 1000 === 0){
+        console.log('junction');
+        queen.junction();
+    }
 }
